1aaa8479ca742cb47c0fa9247e24e01d3dd184cd
daoswap001 <daoswap001@yandex.com>
2020-12-06 23:16:24 +0800

    alert

--- src/utils/index.js
+++ src/utils/index.js
@@ -295,8 +295,8 @@ export const Big = (number) => new BigNumber(number)
 
 export const urls = {
   showTransaction: (tx) => `https://rinkeby.etherscan.io/tx/${tx}/`,
-  showAddress: (address) => `https://www.etherscan.io/address/${address}/`,
-  showToken: (address) => `https://www.etherscan.io/token/${address}/`,
+  showAddress: (address) => `https://rinkeby.etherscan.io/address/${address}/`,
+  showToken: (address) => `https://rinkeby.etherscan.io/token/${address}/`,
   showBlock: (block) => `https://rinkeby.etherscan.io/block/${block}/`,
 }
 
